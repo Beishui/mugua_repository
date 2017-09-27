f08c1996e61af3b2fc9c822b33e1bcafd6178a5b
Beishui <335012293@qq.com>
2017-09-28 03:45:48 +0800

    uu

--- src/com/mugua/Hello.java
+++ src/com/mugua/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 
 	public static void main(String[] args){
 
-		System.out.println("hello mugua Mater mugua1-> mugua2-> mugua2->  111");
+		System.out.println("hello mugua Mater mugua1-> mugua2-> mugua2->  1112222222222");
 	}
 }
